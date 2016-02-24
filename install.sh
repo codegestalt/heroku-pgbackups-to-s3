7cfe07ffc1e7b6ce850a288799bfeec2129ba3c1
Daniel Puglisi <daniel@codegestalt.com>
2016-02-24 09:36:08 +0100

    add install script

---
+++ install.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Creating directory
+echo "Creating directory: /etc/cron.custom"
+mkdir -p /etc/cron.custom
+
+# Downloading keep-alive.sh and save it into /etc/cron.custom directory
+echo "Downloading file 'heroku-pgbackups-to-s3.sh' into /etc/cron.custom"
+curl --silent --output /etc/cron.custom/heroku-pgbackups-to-s3.sh https://raw.githubusercontent.com/codegestalt/heroku-pgbackups-to-s3/master/heroku-pgbackups-to-s3.sh
+
+# Add cronjob to the crontab file
+echo "Adding cronjob to /etc/crontab"
+curl --silent https://raw.github.com/codegestalt/heroku-pgbackups-to-s3/master/crontab >> /etc/crontab
+
+# Set permissions
+echo "Setting permissions for /etc/cron.custom/heroku-pgbackups-to-s3.sh"
+chmod 755 /etc/cron.custom/heroku-pgbackups-to-s3.sh
+
+echo
+echo "Done"
